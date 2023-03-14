65197dac45fb3f8bad458a86a19ffcfd73cb602c
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-03-14 08:58:44 +0530

    add broadcast logic

--- push.js
+++ push.js
@@ -10,3 +10,19 @@ const websocket = new WebSocketServer({ httpServer: httpServer });
 httpServer.listen(8080, () => {
   console.log("Listening on port 8080");
 });
+
+websocket.on("request", (request) => {
+  const connection = request.accept(null, request.origin);
+
+  connection.on("message", (message) => {
+    connections.forEach((c) => {
+      c.send(`User ${connection.socket.remotePort} says : ${message}`);
+    });
+  });
+
+  connections.push(connection);
+
+  connections.forEach((c) => {
+    c.send(`User ${connection.socket.remotePort} just joined.`);
+  });
+});
