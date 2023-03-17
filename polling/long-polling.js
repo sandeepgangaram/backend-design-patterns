3f146b49632e0c0b92279bbee7fad9d282503d63
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-03-17 15:38:40 +0530

    add route handlers

--- polling/long-polling.js
+++ polling/long-polling.js
@@ -1,2 +1,19 @@
 const express = require("express");
+
 const app = express();
+
+const jobs = {};
+
+app.post("/task", (req, res) => {
+  const jobId = `${Date.now()}`;
+  jobs[jobId] = 0;
+  updateJob(jobId);
+  res.end("\n\n" + jobId + "\n\n");
+});
+
+app.get("/checkstatus", async (req, res) => {
+  const { id } = req.query;
+
+  while ((await checkJobComplete(id)) == false);
+  res.end(`${id} Job Status Complete : ${jobs[id]}`);
+});
