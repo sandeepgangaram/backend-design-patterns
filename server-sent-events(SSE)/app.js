2a9b485a66b2ca27f115d34c9523eb3dd77e759f
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-03-17 20:04:58 +0530

    add descriptive comments

--- server-sent-events(SSE)/app.js
+++ server-sent-events(SSE)/app.js
@@ -13,8 +13,13 @@ app.get("/stream", (req, res) => {
 
 let i = 0;
 
+//Note the client side connection doesn't necessariy end here
+//client still expects through event source
+//not a reliable way to code but only to visualize the concept
 function sendStream(res) {
-  if (i <= 10) {
+  if (i <= 100) {
+    //the data format must be matched
+    //especially at start "data " and at end "\n\n"
     res.write("data: " + `hello from server --- ${i++}` + "\n\n");
     setTimeout(() => {
       sendStream(res);
